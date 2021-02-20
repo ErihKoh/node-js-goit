1f6f8da2b57abced7335803f29febcbaa2a1d755
Yurii Zubar <barsuk0831@gmail.com>
2021-02-20 22:00:52 +0200

    added .env

--- routes/api/weather.js
+++ routes/api/weather.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const got = require("got");
 const router = express.Router();
+require("dotenv").config();
 
-router.get("/", function (req, res) {
-  res.json({ message: "Weather" });
+router.get("/", async (req, res, next) => {
+  const { lat, lon } = req.query;
+  const apiKey = process.env.API_KEY;
+  try {
+    const response = await got(
+      "https://api.openweathermap.org/data/2.5/weather",
+      {
+        searchParams: {
+          lat,
+          lon,
+          appid: apiKey,
+        },
+      }
+    );
+
+    res.json(JSON.parse(response.body));
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
