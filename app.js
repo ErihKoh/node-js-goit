ac6ba28f08aa62e2c75b0d4a2695ff1fe5e9102c
Yurii Zubar <barsuk0831@gmail.com>
2021-02-20 21:30:57 +0200

    added logger

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const logger = require("morgan");
+
 const app = express();
+const weatherRouter = require("./routes/api/weather");
+
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/weather", weatherRouter);
 
 module.exports = app;
