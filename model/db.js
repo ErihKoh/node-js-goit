c7dc20346dbe5fe41cb8faa24aebc1d750d8a1e2
Yurii Zubar <barsuk0831@gmail.com>
2021-02-25 21:56:58 +0200

    created server and db

--- model/db.js
+++ model/db.js
@@ -1,9 +1,17 @@
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const uriDB = process.env.URI_DB;
 
-const adapter = new FileSync('./model/cats.json')
-const db = low(adapter)
+const db = MongoClient.connect(uriDB, {
+  useUnifiedTopology: true,
+  poolSize: 5,
+});
 
-db.defaults({ cats: [] }).write()
+process.on("SIGINT", async () => {
+  const client = await db;
+  client.close();
+  console.log("Connect for db closed and app termination");
+  process.exit(1);
+});
 
-module.exports = db
+module.exports = db;
