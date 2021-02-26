fb7882a7aa71376f39d149814a46df1d54705fc8
Yurii Zubar <barsuk0831@gmail.com>
2021-02-26 21:32:44 +0200

    created schema

--- model/Schemas/cat.js
+++ model/Schemas/cat.js
@@ -1,7 +1,33 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const catSchema = new Schema({});
+const catSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for cat"],
+    unique: true,
+  },
+  age: {
+    type: Number,
+    min: 1,
+    max: 25,
+  },
+  isVaccinated: {
+    type: Boolean,
+    default: false,
+  },
+  features: {
+    type: Array,
+    set: (data) => (!data ? [] : data),
+  },
+  date: { type: Date, default: () => Date.now },
+  owner: {
+    name: String,
+    age: Number,
+    address: String,
+  },
+});
 
 const Cat = model("cat", catSchema);
+
 module.exports = Cat;
