5ec5cbcc682c06e18676945586d9485ef75b207e
Yurii Zubar <barsuk0831@gmail.com>
2021-03-05 17:47:09 +0200

    added security

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const helmet = require("helmet");
 const logger = require("morgan");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const { HttpCode } = require("./helpers/constants");
 
 const catsRouter = require("./routes/api/cats");
 const usersRouter = require("./routes/api/users");
@@ -9,10 +12,25 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+app.use(helmet());
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: 10000 }));
 
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  handler: (req, res, next) => {
+    return res.status(HttpCode.BAD_REQUEST).json({
+      status: "error",
+      code: HttpCode.BAD_REQUEST,
+      data: "Bad Request",
+      message: "Too many requests, please try again later",
+    });
+  },
+});
+
+app.use("/api/", apiLimiter);
 app.use("/api/cats", catsRouter);
 app.use("/api/users", usersRouter);
 
