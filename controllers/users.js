c60e94224c8dd857137fbdae14d7e060ba9b5a50
Yurii Zubar <barsuk0831@gmail.com>
2021-03-11 21:18:36 +0200

    added cloud for image

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,25 @@
 const jwt = require("jsonwebtoken");
-const path = require("path");
+// const path = require("path");
 const fs = require("fs").promises;
-const Jimp = require("jimp");
+// const Jimp = require("jimp");
+const { promisify } = require("util");
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+
 const Users = require("../model/users");
 const { HttpCode } = require("../helpers/constants");
-const createFolderIsExist = require("../helpers/create-dir");
+// const createFolderIsExist = require("../helpers/create-dir");
 
-require("dotenv").config();
 const SECRET_WORD = process.env.JWT_SECRET;
-const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS;
+// const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const uploadCloud = promisify(cloudinary.uploader.upload);
 
 const reg = async (req, res, next) => {
   try {
@@ -77,8 +88,11 @@ const logout = async (req, res, next) => {
 const avatars = async (req, res, next) => {
   try {
     const id = req.user.id;
-    const avatarUrl = await saveAvatarToStatic(req);
-    await Users.updateAvatar(id, avatarUrl);
+    const {
+      public_id: imgIdCloud,
+      secure_url: avatarUrl,
+    } = await saveAvatarToCloud(req);
+    await Users.updateAvatar(id, avatarUrl, imgIdCloud);
     return res.json({
       status: "success",
       code: HttpCode.OK,
@@ -91,26 +105,44 @@ const avatars = async (req, res, next) => {
   }
 };
 
-const saveAvatarToStatic = async (req) => {
-  const id = req.user.id;
+// const saveAvatarToStatic = async (req) => {
+//   const id = req.user.id;
+//   const pathFile = req.file.path;
+//   const newNameAvatar = `${Date.now()}-${req.file.originalname}`;
+//   const img = await Jimp.read(pathFile);
+//   await img
+//     .autocrop()
+//     .cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE)
+//     .writeAsync(pathFile);
+//   await createFolderIsExist(path.join(AVATARS_OF_USERS, id));
+//   await fs.rename(pathFile, path.join(AVATARS_OF_USERS, id, newNameAvatar));
+//   const avatarUrl = path.normalize(path.join(id, newNameAvatar));
+//   try {
+//     await fs.unlink(
+//       path.join(process.cwd(), AVATARS_OF_USERS, req.user.avatar)
+//     );
+//   } catch (e) {
+//     console.log(e.message);
+//   }
+//   return avatarUrl;
+// };
+
+const saveAvatarToCloud = async (req) => {
   const pathFile = req.file.path;
-  const newNameAvatar = `${Date.now()}-${req.file.originalname}`;
-  const img = await Jimp.read(pathFile);
-  await img
-    .autocrop()
-    .cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE)
-    .writeAsync(pathFile);
-  await createFolderIsExist(path.join(AVATARS_OF_USERS, id));
-  await fs.rename(pathFile, path.join(AVATARS_OF_USERS, id, newNameAvatar));
-  const avatarUrl = path.normalize(path.join(id, newNameAvatar));
+  const result = await uploadCloud(pathFile, {
+    // public_id: req.user.imgIdCloud?.replace("Photo/", ""),
+    folder: "Photo",
+    transformation: { width: 250, height: 250, crop: "fill" },
+  });
+  cloudinary.uploader.destroy(req.user.imgIdCloud, (err, result) => {
+    console.log(err, result);
+  });
   try {
-    await fs.unlink(
-      path.join(process.cwd(), AVATARS_OF_USERS, req.user.avatar)
-    );
+    await fs.unlink(pathFile);
   } catch (e) {
     console.log(e.message);
   }
-  return avatarUrl;
+  return result;
 };
 
 module.exports = { reg, login, logout, avatars };
--- model/users.js
+++ model/users.js
@@ -17,8 +17,8 @@ const updateToken = async (id, token) => {
   return await User.updateOne({ _id: id }, { token });
 };
 
-const updateAvatar = async (id, avatar) => {
-  return await User.updateOne({ _id: id }, { avatar });
+const updateAvatar = async (id, avatar, imgIdCloud) => {
+  return await User.updateOne({ _id: id }, { avatar, imgIdCloud });
 };
 
 module.exports = {
